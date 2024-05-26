c371eb8a6268428c55ee28079e27c1ff0bc7218a
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2024-05-26 08:21:29 +0200

    Added empty Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,3 @@
+println("gradle.startParameter.taskNames: ${gradle.startParameter.taskNames}")
+System.getProperties().forEach { key, value -> println("System.getProperties(): $key=$value") }
+System.getenv().forEach { (key, value) -> println("System.getenv(): $key=$value") }
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+rootProject.name = "DoorbellMp"
+include(
+)
+
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
