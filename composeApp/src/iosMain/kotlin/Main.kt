48df896ee6dccf2e4bfacad00b39addf56662729
Siarhei Luskanau <siarhei.luskanau@gmail.com>
2025-05-01 19:27:42 +0200

    Update KMP base structure

--- composeApp/src/iosMain/kotlin/Main.kt
+++ composeApp/src/iosMain/kotlin/Main.kt
@@ -2,8 +2,6 @@ import androidx.compose.ui.window.ComposeUIViewController
 import platform.UIKit.UIViewController
 import siarhei.luskanau.doorbell.mp.app.KoinAppComposable
 
-fun mainViewController(): UIViewController = ComposeUIViewController(
-    configure = { enforceStrictPlistSanityCheck = false }
-) {
+fun mainViewController(): UIViewController = ComposeUIViewController {
     KoinAppComposable()
 }
